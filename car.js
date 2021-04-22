20e0af042d57e5617fa1f9a2990ba53fdd783c53
KseniaFalcoz <kseniafalcoz@gmail.com>
2021-04-22 10:04:08 +0200

    modification

--- car.js
+++ car.js
@@ -7,23 +7,19 @@ ctx.fillStyle = "lightgray";
 ctx.arc(150, 300, 45, 0, 2*pi, false);
 ctx.stroke();
 ctx.fill();
-ctx.closePath();
 
 ctx.beginPath();
 ctx.fillStyle = "lightgray";
 ctx.arc(450, 300, 45, 0, 2*pi, false);
 ctx.stroke();
 ctx.fill();
-ctx.closePath();
 
 ctx.beginPath();
 ctx.fillStyle = "green";
 ctx.fillRect(105, 180, 395, 75);
 ctx.fill();
-ctx.closePath();
 
 ctx.beginPath();
 ctx.fillStyle = "green";
 ctx.fillRect(180, 105, 245, 75);
 ctx.fill();
-ctx.closePath();
--- house.js
+++ house.js
@@ -1,11 +1,29 @@
 var canvas = document.getElementById("house");
 var ctx = canvas.getContext("2d");
 
-
-
+ctx.beginPath();
+ctx.fillStyle = "rosybrown";
+ctx.moveTo(130, 180);
+ctx.lineTo(370, 180);
+ctx.lineTo(250, 50);
+ctx.fill();
 
 ctx.beginPath();
 ctx.fillStyle = "lightsteelblue";
-ctx.fillRect(130, 180, 240, 300);
+ctx.fillRect(130, 180, 240, 250);
 ctx.fill();
-ctx.closePath();
+
+ctx.beginPath();
+ctx.fillStyle = "lightslategray";
+ctx.fillRect(150, 200, 60, 60);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "lightslategray";
+ctx.fillRect(290, 200, 60, 60);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "lightslategray";
+ctx.fillRect(210, 320, 80, 110);
+ctx.fill();
